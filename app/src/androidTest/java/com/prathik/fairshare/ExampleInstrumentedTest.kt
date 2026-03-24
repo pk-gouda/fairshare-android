0d9633885eb6d1cd18242cb9a8ed245320ebd499
pk-gouda <goudaprathik@gmail.com>
2026-03-24 17:04:36 -0500

    chore: project setup + dependencies configured

--- app/src/androidTest/java/com/prathik/fairshare/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/prathik/fairshare/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.prathik.fairshare_android", appContext.packageName)
+        assertEquals("com.prathik.fairshare", appContext.packageName)
     }
 }
